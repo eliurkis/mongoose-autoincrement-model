301975a6951b9272541f27dd0818d2cb6ef8b1f9
Eliurkis Diaz <me@eliurkis.com>
2018-03-03 01:52:22 -0500

    Fix issue with count

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ exports.plugin = function (schema, options) {
               function (err, updatedIdentityCounter) {
                 if (err) return next(err);
                 // If there are no errors then go ahead and set the document's field to the current count.
-                doc[settings.field] = updatedIdentityCounter.count;
+                doc[settings.field] = updatedIdentityCounter && updatedIdentityCounter.count;
                 // Continue with default document save functionality.
                 next();
               }
